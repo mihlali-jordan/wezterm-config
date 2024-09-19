f880b07d1a89eb5a40229a4f20dabea2e580363a
mihlali-jordan <biz@jordanmihlali.dev>
2024-09-19 22:13:34 +0200

    initial commit

---
+++ wezterm.lua
@@ -0,0 +1,188 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+-- config.color_scheme = "Synthwave (Gogh)"
+-- config.color_scheme = "Tokyo Night"
+-- config.color_scheme = "Banana Blueberry"
+-- config.color_scheme = "Rose Pine"
+-- config.color_scheme = "Horizon Dark (base16)"
+-- config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Github (base16)"
+-- config.color_scheme = "Github Light (Gogh)"
+
+config.font = wezterm.font("Berkeley Mono")
+
+config.font_size = 13
+
+config.window_decorations = "RESIZE"
+
+config.enable_tab_bar = true
+
+config.window_background_opacity = 0.8
+
+config.macos_window_background_blur = 0
+
+config.window_padding = {
+	top = 64,
+	bottom = 0,
+	left = 0,
+	right = 0,
+}
+
+config.keys = {
+	-- Toggle fullscreen
+	{
+		key = "f",
+		mods = "CTRL|CMD",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
+
+config.colors = {
+	tab_bar = {
+		background = "#ffffff",
+		active_tab = {
+			fg_color = "#ffffff",
+			bg_color = "#f43f5e",
+		},
+		inactive_tab = {
+			fg_color = "#f43f5e",
+			bg_color = "#fff1f2",
+		},
+		new_tab = {
+			fg_color = "#f43f5e",
+			bg_color = "#fff1f2",
+		},
+	},
+}
+
+-- tmux
+config.leader = { key = "q", mods = "ALT", timeout_milliseconds = 2000 }
+config.keys = {
+	{
+		mods = "LEADER",
+		key = "c",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		key = "r",
+		mods = "LEADER",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	{
+		mods = "LEADER",
+		key = "x",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		mods = "LEADER",
+		key = "b",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		mods = "LEADER",
+		key = "n",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		mods = "LEADER",
+		key = "|",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "LEADER",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "LEADER",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "LEADER",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		mods = "LEADER",
+		key = "LeftArrow",
+		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "RightArrow",
+		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "DownArrow",
+		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "UpArrow",
+		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+	},
+}
+
+for i = 0, 9 do
+	-- leader + number to activate that tab
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i),
+	})
+end
+
+-- tab bar
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_and_split_indices_are_zero_based = true
+
+-- tmux status
+wezterm.on("update-right-status", function(window, _)
+	local SOLID_LEFT_ARROW = ""
+	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+	local prefix = ""
+
+	if window:leader_is_active() then
+		prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	end
+
+	if window:active_tab():tab_id() ~= 0 then
+		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+	end -- arrow color based on if tab is first pane
+
+	window:set_left_status(wezterm.format({
+		{ Background = { Color = "#b7bdf8" } },
+		{ Text = prefix },
+		ARROW_FOREGROUND,
+		{ Text = SOLID_LEFT_ARROW },
+	}))
+end)
+
+return config
